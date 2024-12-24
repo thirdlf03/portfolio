ee29502cdbc4cd162e8a38bb9b80566e35f4019c
thirdlf03 <thirdlf03@gmail.com>
2024-12-24 23:54:54 +0900

    deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
     <>
       <h1>Hi 👋</h1>
       <div>
-        Can't wait to see what you build with qwik!
+        Happy Happy !
         <br />
         Happy coding.
       </div>
