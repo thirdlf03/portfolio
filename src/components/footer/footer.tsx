dc7e5692259e44a56c2f68022402aef2faa37732
thirdlf03 <thirdlf03@gmail.com>
2024-12-28 02:56:01 +0900

    add aria-label

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,17 +10,17 @@ export default component$(() => {
                 <div class="icon-text">// CONTACT.md</div>
                 <div class="icon-container">
                     <div class="icon-content">
-                        <a href="https://github.com/thirdlf03" target="_">
+                        <a href="https://github.com/thirdlf03" target="_" aria-label="GitHubのリンクです。">
                             <div style={{color: "#d1d5db", fontSize: "24px"}}><LuGithub/></div>
                         </a>
                     </div>
                     <div class="icon-content">
-                        <a href="https://zenn.dev/thirdlf" target="_">
+                        <a href="https://zenn.dev/thirdlf" target="_" aria-label="Zennのリンクです。">
                             <div style={{color: "#d1d5db", fontSize: "24px"}}><LuBook/></div>
                         </a>
                     </div>
                     <div class="icon-content">
-                        <a href="https://x.com/thirdlf1" target="_">
+                        <a href="https://x.com/thirdlf1" target="_" aria-label="Xのリンクです。">
                             <div style={{color: "#d1d5db", fontSize: "24px"}}><LuX/></div>
                         </a>
                     </div>
