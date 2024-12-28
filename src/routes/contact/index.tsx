d01f3d48df8f05da63bd171f3f396b26b97d3c48
thirdlf03 <thirdlf03@gmail.com>
2024-12-29 04:14:26 +0900

    complete contact form

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -2,19 +2,43 @@ import {component$, useSignal, useStylesScoped$, useTask$} from "@builder.io/qwi
 import styles from "./style.css?inline";
 import {Form, routeAction$} from "@builder.io/qwik-city";
 import {LuSend} from "@qwikest/icons/lucide";
+import {createServerClient} from "supabase-auth-helpers-qwik";
 
 
-export const useContact = routeAction$(async (props) => {
-    // Todo ちゃんとフォーム実装しようね
-    console.log(props);
+export const useContact = routeAction$(async (props, requestEv) => {
+    const supabaseClient = createServerClient(
+        requestEv.env.get("PUBLIC_SUPABASE_URL")!,
+        requestEv.env.get("PUBLIC_SUPABASE_ANON_KEY")!,
+        requestEv
+    );
     const {name, email, message} = props;
-    console.log("hello tom");
-    console.log(name, email, message);
+
+    await supabaseClient.from("contact").insert({ name: name, email: email, message: message });
+
+    await fetch(requestEv.env.get("PUBLIC_DISCORD_WEBHOOK_URL")!, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            content: `<@1072626931012472852> 新しいお問い合わせだよ！\n${name} さんからのメッセージ！`,
+            embeds: [
+                {
+                    title: "問い合わせ",
+                    description: `お名前: ${name}\nメールアドレス: ${email}\nメッセージ: ${message}`,
+                    color: 0x00ff00,
+                },
+            ],
+        }),
+    });
 })
 
 export default component$(() => {
     useStylesScoped$(styles)
+    const name = useSignal<string>("");
     const email = useSignal<string>("");
+    const message = useSignal<string>("");
+
     const isValid = useSignal<boolean>(true);
     const Contact = useContact();
     const reg = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
@@ -26,7 +50,6 @@ export default component$(() => {
             isValid.value = false;
         }
         else{
-            console.log(Math.random());
             isValid.value = true;
         }
     });
@@ -37,16 +60,21 @@ export default component$(() => {
                 <div class="title">
                     // CONTACT_FORM.bash
                 </div>
-                <Form action={Contact}>
+                <Form action={Contact} onSubmit$={async () => {
+                        name.value = "";
+                        email.value = "";
+                        message.value = "";
+                        window.alert("Message sent successfully!");
+                    }}>
                     <div class="form-inside-container">
                         <div class="form-input">
-                            <span>$ </span><input name="name" placeholder="enter_name" required/>
+                            <span>$ </span><input name="name" placeholder="enter_name" required bind:value={name}/>
                         </div>
                         <div class="form-input">
                             <span>$ </span><input name="email" placeholder="enter_email" required bind:value={email}/>
                         </div>
                         <div class="form-textarea">
-                            <span>$ </span><textarea id="message" name="message" required placeholder="type_your_message"></textarea>
+                            <span>$ </span><textarea id="message" name="message" required placeholder="type_your_message" bind:value={message}></textarea>
                         </div>
                         <div>
                             <button disabled={isValid.value}><LuSend style={{marginRight: "10px"}}/>send</button>
