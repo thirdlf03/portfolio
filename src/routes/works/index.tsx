be4424dd0fe4565044b1af2c3b66a83c9734fbd6
thirdlf03 <thirdlf03@gmail.com>
2025-01-16 22:21:10 +0900

    atcodersetup追加

--- src/routes/works/index.tsx
+++ src/routes/works/index.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
         <>
             <div class="main-container">
                 <div class="title">
-                    <h1>Atode Tyanto Tukuru...</h1>
+                    <h1>My Works</h1>
                 </div>
             </div>
             <div class="work-container">
@@ -15,7 +15,7 @@ export default component$(() => {
                     {title: "3段クルーン", describe: "https://zawa.thirdlf03.com"},
                     {title: "QRコード神経衰弱", describe: "https://topaz.dev/projects/a939290be6545eff5895"},
                     {title: "刹那", describe: "https://topaz.dev/projects/575f49eb3bc26cf370b1"},
-                    {title: "ダミー4", describe: "なんか"},
+                    {title: "AtCoder環境構築", describe: "https://github.com/thirdlf03/setup-atcoder"},
                 ].map((item, index) => (
                     <div key={index} class="work-card">
                         <div class="work-card-header">
